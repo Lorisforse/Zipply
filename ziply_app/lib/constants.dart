0aecab8a18e8b8d2eeacc7497efe38a14157a78f
Loris Amato <lorisamato97@yahoo.it>
2026-06-11 22:28:58 +0200

    feat(auth): login e registrazione utente - backend Go + Flutter

--- ziply_app/lib/constants.dart
+++ ziply_app/lib/constants.dart
@@ -1,4 +1,4 @@
 // Costanti globali dell'applicazione Ziply.
 
-const String kBaseUrl = 'http://10.0.2.2:8080'; // emulatore Android
+const String kBaseUrl = 'https://api.lorisamato.it/ziply/api';
 const String kTokenKey = 'ziply_auth_token';
